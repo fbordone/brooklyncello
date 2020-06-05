d5dc904bea78383a237c805fecfe3a06dd0df234
Frankie Bordone <bordone.francesco@gmail.com>
2020-06-04 23:34:28 -0400

    Remove general tab from recipe drilldown backend ACF

--- wp-content/themes/brooklyncello/resources/views/modules/featured.blade.php
+++ wp-content/themes/brooklyncello/resources/views/modules/featured.blade.php
@@ -18,10 +18,7 @@
           <div class="recipe featured__recipe">
             <a class="recipe__wrap" href="{{ get_permalink($related_recipe_ids[$i]) }}">
               <figure class="recipe__img-wrap">
-                <img class="recipe__img"
-                  srcset="{{ wp_get_attachment_image_srcset( get_field('archive_recipe_thumbnail', $related_recipe_ids[$i]), 'medium') }}"
-                  sizes="(min-width: 80em) 30em, (min-width: 60em) 50vw, 100vw"
-                  alt="{{ get_post_meta( get_field('archive_recipe_thumbnail', $related_recipe_ids[$i]), '_wp_attachment_image_alt', true) }}">
+                {!! get_the_post_thumbnail($related_recipe_ids[$i]) !!}
               </figure>
 
               <h3 class="recipe__header">{{ get_the_title($related_recipe_ids[$i]) }}</h3>
